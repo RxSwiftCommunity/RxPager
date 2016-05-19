92b2b2b7f6fdd877a1598fdbfbf2aa119730e831
Pierre-Guillaume Herveou <pgherveou@gmail.com>
2016-05-19 11:49:21 -0700

    add test to check if next not called to many time

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -13,8 +13,15 @@ struct Page {
 
 // MARK: globals
 
+/// delay block after `time` seconds
+func delay(time: Double, block: () -> Void) {
+  dispatch_after(
+    dispatch_time(DISPATCH_TIME_NOW, Int64(time * Double(NSEC_PER_SEC))),
+    dispatch_get_main_queue(), block)
+}
+
 /// page factory helper
-func getPager() -> Pager<Page> {
+func createPager() -> Pager<Page> {
   return Pager(
     paging: { (previousPage: Page?) -> Observable<Page> in
       let last = previousPage?.values.last ?? 0
@@ -29,56 +36,69 @@ func getPager() -> Pager<Page> {
   )
 }
 
+func createASyncPager() -> Pager<Page> {
+  return Pager(
+    paging: { (previousPage: Page?) -> Observable<Page> in
+      let last = previousPage?.values.last ?? 0
+      return Observable.just(Page(
+        values: [last + 1, last + 2, last + 3],
+        hasNext: last + 3 < 10)
+        ).delaySubscription(0.1, scheduler: MainScheduler.instance)
+    },
+    hasNext: { (page: Page?) -> Bool in
+      return page?.hasNext == true
+    }
+  )
+}
+
 class Tests: XCTestCase {
 
-  var subscription: Disposable?
+  var disposeBag = DisposeBag()
 
   override func tearDown() {
-    subscription?.dispose()
+    disposeBag = DisposeBag()
   }
 
   func testGetFirstPage() {
     let expectation = expectationWithDescription("get first page")
-    let pager = getPager()
-    subscription = pager.page
+    let pager = createPager()
+    pager.page
       .subscribeNext { page in
-      XCTAssertEqual(page.values, [1, 2, 3])
-      expectation.fulfill()
-    }
-
-    pager.next()
+        XCTAssertEqual(page.values, [1, 2, 3])
+        expectation.fulfill()
+      }
+      .addDisposableTo(disposeBag)
 
     waitForExpectationsWithTimeout(1, handler: nil)
   }
 
   func testGetSecondPage() {
     let expectation = expectationWithDescription("get first two page")
-    let pager = getPager()
+    let pager = createPager()
 
-    subscription = pager.page
+    pager.page
       .skip(1)
       .subscribeNext { page in
         XCTAssertEqual(page.values, [4, 5, 6])
         expectation.fulfill()
-    }
+      }
+      .addDisposableTo(disposeBag)
 
-    pager.next()
     pager.next()
     waitForExpectationsWithTimeout(1, handler: nil)
   }
 
   func testGetThirdPage() {
     let expectation = expectationWithDescription("get first three page")
-    let pager = getPager()
+    let pager = createPager()
 
-    subscription = pager.page
+    pager.page
       .skip(2)
       .subscribeNext { page in
         XCTAssertEqual(page.values, [7, 8, 9])
         expectation.fulfill()
-    }
+      }.addDisposableTo(disposeBag)
 
-    pager.next()
     pager.next()
     pager.next()
     waitForExpectationsWithTimeout(1, handler: nil)
@@ -86,17 +106,38 @@ class Tests: XCTestCase {
 
   func testCompletePager() {
     let expectation = expectationWithDescription("get completed event")
-    let pager = getPager()
+    let pager = createPager()
 
-    subscription = pager.page
+    pager.page
       .subscribeCompleted { _ in
         expectation.fulfill()
-    }
+      }
+      .addDisposableTo(disposeBag)
 
-    pager.next() // [1, 2 ,3]
+    // starts with [1, 2 ,3]
     pager.next() // [4, 5, 6]
     pager.next() // [7, 8, 9]
     pager.next() // [10, 11, 12], completed
+    waitForExpectationsWithTimeout(1, handler: nil)
+  }
+
+  func testCantNextMoreThanOnceBeforeNextPage() {
+    let expectation = expectationWithDescription("get first two page")
+    let pager = createASyncPager()
+
+    pager.page
+      .subscribeNext { page in
+        // should only be called once
+        XCTAssertEqual(page.values, [1, 2, 3])
+
+        // wait for non event and fulfill
+        delay(0.2) { expectation.fulfill() }
+      }
+      .addDisposableTo(disposeBag)
+
+    pager.next()
+    pager.next() // should be a noop
+    pager.next() // should be a noop
     waitForExpectationsWithTimeout(1, handler: nil)
   }
   
