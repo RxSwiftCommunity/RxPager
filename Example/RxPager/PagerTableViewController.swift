0bfd26548ae6ad68de9780b6722b8a464c37b7ec
Pierre-Guillaume Herveou <pgherveou@gmail.com>
2016-06-14 09:46:45 -0700

    update example

--- Example/RxPager/PagerTableViewController.swift
+++ Example/RxPager/PagerTableViewController.swift
@@ -1,6 +1,14 @@
 import UIKit
 import RxSwift
+import RxCocoa
 import RxPager
+
+// MARK: Helper
+
+private let startLoadingOffset: CGFloat = 20.0
+private func isNearTheBottomEdge(contentOffset: CGPoint, _ tableView: UITableView) -> Bool {
+  return contentOffset.y + tableView.frame.size.height + startLoadingOffset > tableView.contentSize.height
+}
 
 // MARK: PagerTableViewController
 
@@ -16,7 +24,17 @@ class PagerTableViewController: UITableViewController {
     }
   }
 
-  /// loading indicator, placed in the tableView footer 
+  /// pager trigger
+  private lazy var loadNextPageTrigger: Observable<Void> = {
+    return self.tableView.rx_contentOffset
+      .flatMap { (offset) -> Observable<Void> in
+        isNearTheBottomEdge(offset, self.tableView)
+          ? Observable.just()
+          : Observable.empty()
+    }
+  }()
+
+  /// loading indicator, placed in the tableView footer
   /// the indicator animate until the page stream complete
   private lazy var activityIndicator: UIActivityIndicatorView = {
     let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
@@ -26,17 +44,20 @@ class PagerTableViewController: UITableViewController {
   }()
 
   /// Pager, that emit pages of [Int], and complete when last emitted int is greater than 100
-  let pager: Pager<[Int]> = Pager(
-    paging: { (previousPage: [Int]?) -> Observable<[Int]> in
+  private lazy var page: Observable<[Int]> = {
+    func paging(previousPage: [Int]?) -> Observable<[Int]> {
       let last = previousPage?.last ?? 0
       return Observable
         .just(Array(1...20).map { last + $0 })
         .delaySubscription(0.5, scheduler: MainScheduler.instance)
-    },
-    hasNext: { (page: [Int]?) -> Bool in
+    }
+
+    func hasNext(page: [Int]?) -> Bool {
       return page?.last < 100
     }
-  )
+
+    return rx_pager(paging: paging, hasNext: hasNext, trigger: self.loadNextPageTrigger)
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -45,7 +66,7 @@ class PagerTableViewController: UITableViewController {
     activityIndicator.startAnimating()
 
     // scan, and update dataSource
-    pager.page
+    page
       .scan([Int](), accumulator: +)
       .subscribeNext { [weak self] in
         self?.dataSource = $0
@@ -53,8 +74,7 @@ class PagerTableViewController: UITableViewController {
       .addDisposableTo(disposeBag)
 
     // update dataSource when the stream complete
-    pager
-      .page
+    page
       .subscribeCompleted { [weak self] in
         self?.activityIndicator.stopAnimating()
       }
@@ -71,12 +91,6 @@ class PagerTableViewController: UITableViewController {
     guard let cell = tableView.dequeueReusableCellWithIdentifier("pagerCell") else { fatalError() }
     cell.textLabel?.text = "Row \(dataSource[indexPath.row])"
     return cell
-  }
-
-  override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
-    if indexPath.row == dataSource.count - 1 {
-      pager.next()
-    }
-  }
+  }  
 }
 
